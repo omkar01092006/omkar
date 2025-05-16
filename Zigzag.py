f853b97db0fe3929b0ed7cf3537036e54263782b
omkar01092006 <omkarp.24.beai@acharya.ac.in>
2025-05-16 11:42:01 +0530

    Create  Zigzag.py

---
+++ Zigzag.py
@@ -0,0 +1,27 @@
+def print_zigzag(rows, columns):
+    
+    for i in range(rows):
+        for j in range(columns):
+            
+            if (i + j) % 2 == 0:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print() 
+
+def main():
+    print("Zigzag Pattern Generator")
+    
+   
+    try:
+        rows = int(input("Enter the number of rows: "))
+        columns = int(input("Enter the number of columns: "))
+        
+       
+        print_zigzag(rows, columns)
+    
+    except ValueError:
+        print("Please enter valid integer values for rows and columns.")
+
+
+main()
