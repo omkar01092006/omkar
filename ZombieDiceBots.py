2e00a0963428a453ad4c4cc4d534b2838d39b468
omkar01092006 <omkarp.24.beai@acharya.ac.in>
2025-05-26 00:48:26 +0530

    Create ZombieDiceBots.py

---
+++ ZombieDiceBots.py
@@ -0,0 +1,73 @@
+import zombiedice
+import random
+
+class RandomBot:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        while True:
+            roll = zombiedice.roll()
+            if roll is None:
+                break
+            if random.random() < 0.5:
+                break
+
+class StopAfter2BrainsBot:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        brains = 0
+        while True:
+            roll = zombiedice.roll()
+            if roll is None:
+                break
+            for d in roll:
+                if d['brain']:
+                    brains += 1
+            if brains >= 2:
+                break
+
+class StopAfterShotgunBot:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        shotguns = 0
+        while True:
+            roll = zombiedice.roll()
+            if roll is None:
+                break
+            for d in roll:
+                if d['shotgun']:
+                    shotguns += 1
+            if shotguns >= 1:
+                break
+
+class RiskyBot:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        brains = 0
+        shotguns = 0
+        while shotguns < 2:
+            roll = zombiedice.roll()
+            if roll is None:
+                break
+            for d in roll:
+                brains += d['brain']
+                shotguns += d['shotgun']
+
+class RollMax4TimesBot:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        rolls = 0
+        while rolls < 4:
+            roll = zombiedice.roll()
+            if roll is None:
+                break
+            rolls += 1
